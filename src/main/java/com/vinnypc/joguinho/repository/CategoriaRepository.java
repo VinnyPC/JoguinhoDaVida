7f6f1713fec384e45c63ac4fb464e510a4204936
VinnyPC <vinisilvax3@gmail.com>
2023-08-03 12:40:18 -0300

    feat: CRUD categoria

--- src/main/java/com/vinnypc/joguinho/repository/CategoriaRepository.java
+++ src/main/java/com/vinnypc/joguinho/repository/CategoriaRepository.java
@@ -1,9 +1,15 @@
 package com.vinnypc.joguinho.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
 
 import com.vinnypc.joguinho.model.Categoria;
 
+
 public interface CategoriaRepository extends JpaRepository<Categoria, Long> {
+	
+	public List <Categoria> findAllByNomeContainingIgnoreCase(@Param("nome") String nome);
 
 }
